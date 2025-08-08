9923e3b23f5b182893d71e5fb28dea1b35a11366
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-08 18:43:57 +0800

    define home route location

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public const HOME = "/posts";
+
     /**
      * Register any application services.
      */
