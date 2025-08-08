47df54771a9cf258a654583d33da5bea5baa0abc
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-08 19:31:55 +0800

    add LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\AppServiceProvider;
+use Illuminate\Support\Facades\Redirect;
+
+class LoginController extends Controller
+{
+    public function create()
+    {
+        return Inertia::render('Auth/Login');
+    }
+
+    public function store(Request $request)
+    {
+        // 1. Validate credentials
+        $credentials = $request->validate([
+            "email" => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // 2. Login Process
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return Redirect::intended(AppServiceProvider::HOME);
+        }
+
+        return Redirect::back()->with('error', 'Error given credentials');
+    }
+}
