b7dbaafe40645fa527c8ad5b0705eb24905a0d85
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-08 19:37:23 +0800

    add route resource for LoginController and RegisterController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -14,4 +16,14 @@ Route::controller(PostController::class)->group(function () {
     Route::get('/posts/{post}/edit', 'edit')->name('posts.edit');
     Route::put('/posts/{post}', 'update')->name('posts.update');
     Route::delete('/posts/{post}', 'destroy')->name('posts.destroy');
+});
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'create')->name('login.create');
+    Route::post('/login', 'store')->name('login.store');
+});
+
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('/register', 'create')->name('register.create');
+    Route::post('/register', 'store')->name('register.store');
 });
