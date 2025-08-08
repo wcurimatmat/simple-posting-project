2a25ac778a036a22adac45cd009c980bd3a96a5a
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-08 18:51:17 +0800

    add Login page

---
+++ resources/js/Pages/Auth/Login.jsx
@@ -0,0 +1,69 @@
+import { useForm } from "@inertiajs/react";
+
+function Login() {
+    const { data, setData, post, processing } = useForm({
+        email: "",
+        password: "",
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        post("/login");
+    }
+
+    return (
+        <>
+            <header className="p-14">
+                <p className="font-bold">LaReact Postings</p>
+            </header>
+
+            <section className="px-14">
+                <header className="mb-6">
+                    <h1 className="text-2xl font-bold">Login</h1>
+                </header>
+
+                <form
+                    onSubmit={handleSubmit}
+                    className="m-auto grid w-lg gap-6"
+                >
+                    <div className="field">
+                        <label htmlFor="" className="font-bold text-gray-600">
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            id="title"
+                            value={data.email}
+                            className="input"
+                            onChange={(e) => setData("email", e.target.value)}
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="" className="font-bold text-gray-600">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="title"
+                            value={data.password}
+                            className="input"
+                            onChange={(e) =>
+                                setData("password", e.target.value)
+                            }
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="submit"
+                        disabled={processing}
+                    >
+                        Login
+                    </button>
+                </form>
+            </section>
+        </>
+    );
+}
+
+export default Login;
