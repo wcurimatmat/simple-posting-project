2a375babc82384431f855951b338bfd4debbbdfa
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-31 15:40:42 +0800

    refactor: extract codes to make a dedicated function

--- resources/js/Pages/Index/Index.jsx
+++ resources/js/Pages/Index/Index.jsx
@@ -6,33 +6,7 @@ import { Link, router, usePage } from "@inertiajs/react";
 import FlashMessage from "../../Components/FlashMessage";
 
 function Index({ posts }) {
-    const [postListing, setPostListing] = useState([]);
     const { flash, error } = usePage().props;
-
-    useEffect(() => {
-        setPostListing(posts);
-    }, []);
-
-    usePostEvents(setPostListing);
-
-    function handleDelete(id) {
-        if (confirm("Are you sure you want to delete this post?")) {
-            // router.visit(`/posts/${id}`, {
-            //     method: "delete",
-            //     preserveScroll: true,
-            // });
-
-            router.delete(`/posts/${id}`, {
-                onSuccess: function () {
-                    setPostListing(function (previousPostListings) {
-                        return previousPostListings.filter(function (post) {
-                            return post.id !== id;
-                        });
-                    });
-                },
-            });
-        }
-    }
 
     return (
         <UserLayout>
@@ -41,56 +15,104 @@ function Index({ posts }) {
             </header>
 
             <section>
-                <ul className="flex flex-col gap-6">
-                    {postListing.map((post) => (
-                        <li
-                            key={post.id}
-                            className="flex w-lg shrink grow basis-4 justify-between border border-gray-400 p-8"
-                        >
-                            <div className="grid gap-3">
-                                <Link href={route("posts.show", post.id)}>
-                                    <h2 className="text-2xl font-bold">
-                                        {post.title}
-                                    </h2>
-                                </Link>
-                                <p>{post.content}</p>
-                                <p className="text-sm text-gray-400">
-                                    {new Date(post.updated_at).toLocaleString()}
-                                </p>
-                            </div>
-
-                            <div className="">
-                                <ul className="align-center flex gap-4">
-                                    <li>
-                                        <form
-                                            onSubmit={(e) => {
-                                                e.preventDefault();
-                                                handleDelete(post.id);
-                                            }}
-                                        >
-                                            <button type="submit">
-                                                <Trash2 size={20} />
-                                            </button>
-                                        </form>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            href={route("posts.edit", {
-                                                post: post.id,
-                                            })}
-                                        >
-                                            <Pencil size={20} />
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                <PostListings posts={posts} />
             </section>
 
             {flash.success && <FlashMessage message={flash.success} />}
         </UserLayout>
+    );
+}
+
+function EmptyPostListings() {
+    return (
+        <div className="space-y-4">
+            <p>Empty!</p>
+
+            <Link
+                href={route("posts.create")}
+                className="font-bold text-blue-400 underline"
+            >
+                Try creating a new post
+            </Link>
+        </div>
+    );
+}
+
+function PostListings({ posts }) {
+    const [listOfPosts, setListOfPosts] = useState([]);
+
+    useEffect(() => {
+        setListOfPosts(posts);
+    }, [posts]);
+
+    usePostEvents(setListOfPosts);
+
+    function handleDelete(id) {
+        if (!confirm("Are you sure you want to delete this post?")) return;
+
+        // router.visit(`/posts/${id}`, {
+        //     method: "delete",
+        //     preserveScroll: true,
+        // });
+
+        router.delete(`/posts/${id}`, {
+            onSuccess: function () {
+                setListOfPosts(function (previousPostListings) {
+                    return previousPostListings.filter(function (post) {
+                        return post.id !== id;
+                    });
+                });
+            },
+        });
+    }
+
+    if (!listOfPosts || listOfPosts.length === 0) {
+        return <EmptyPostListings />;
+    }
+
+    return (
+        <ul className="grid grid-cols-2 gap-6">
+            {listOfPosts.map((post) => (
+                <li
+                    key={post.id}
+                    className="flex w-full justify-between border border-gray-400 p-8"
+                >
+                    <div className="grid gap-3">
+                        <Link href={route("posts.show", post.id)}>
+                            <h2 className="text-2xl font-bold">{post.title}</h2>
+                        </Link>
+                        <p>{post.content}</p>
+                        <p className="text-sm text-gray-400">
+                            {new Date(post.updated_at).toLocaleString()}
+                        </p>
+                    </div>
+
+                    <div className="">
+                        <ul className="align-center flex gap-4">
+                            <li>
+                                <button
+                                    className="cursor-pointer"
+                                    onClick={() => {
+                                        handleDelete(post.id);
+                                    }}
+                                >
+                                    <Trash2 size={20} />
+                                </button>
+                            </li>
+                            <li>
+                                <Link
+                                    href={route("posts.edit", {
+                                        post: post.id,
+                                    })}
+                                >
+                                    <Pencil size={20} />
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            ))}
+        </ul>
     );
 }
 
