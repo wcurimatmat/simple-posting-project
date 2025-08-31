41770bf9085d9f1842f98b85d1e87f712e8c45d9
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-31 14:10:43 +0800

    add related method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,14 @@ class Post extends Model
 
     protected $table = "posts";
     protected $fillable = ["title", "content"];
+
+    public function related($limit = 4)
+    {
+        $related = Post::where('id', '!=', $this->id)
+            ->inRandomOrder()
+            ->take($limit)
+            ->get();
+
+        return $related;
+    }
 }
